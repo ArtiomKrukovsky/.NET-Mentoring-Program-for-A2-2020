2204106796fa59f11137bc24904b50cfbebba256
Artiom Krukovsky <Artiomgood1@gmail.com>
2020-02-23 22:25:45 +0300

    Module 3 - Task 1

---
+++ ASP.NET Core/Module 3 - Task 1/Module 3 - Task 1/Models/CustomerCustomerDemo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Module3
+{
+    public partial class CustomerCustomerDemo
+    {
+        public string CustomerId { get; set; }
+        public string CustomerTypeId { get; set; }
+
+        public Customers Customer { get; set; }
+        public CustomerDemographics CustomerType { get; set; }
+    }
+}
---
+++ ASP.NET Core/Module 3 - Task 1/Module 3 - Task 1/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Module_3___Task_1.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
